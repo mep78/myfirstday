c450fb7be7a1aee6112cfff8dcf100a03930cb65
mep78 <maria.ponce78@gmail.com>
2022-02-06 21:08:11 -0300

    function talk

---
+++ myfirstfunctionjavascript.js
@@ -0,0 +1,11 @@
+function registerJoey (){
+    function talk (palabra){
+    return palabra 
+ }
+ talk ("hello, how are you, what do you need")
+ talk ("hello, i need to register my daughter in the school")
+ talk ("ok, give me her name")
+ talk ("her name is joey")
+ talk ("ok, thank you")
+}
+registerJoey ()
